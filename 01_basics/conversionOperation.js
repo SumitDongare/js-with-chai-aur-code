4fe1fdda284bfb23766c95d4e41d11edc0a1a501
SumitDongare <110616406+SumitDongare@users.noreply.github.com>
2023-12-21 08:38:08 +0000

    forOperation true

---
+++ 01_basics/conversionOperation.js
@@ -0,0 +1,19 @@
+// ****** Operations *********
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**3)
+// console.log(2/3)
+// console.log(2%3)
+
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+2+2);
+// console.log(1+2+"2");
+
+
