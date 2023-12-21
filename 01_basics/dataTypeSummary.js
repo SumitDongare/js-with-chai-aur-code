4144953b56b806add7031848648b9bcb32efed91
SumitDongare <110616406+SumitDongare@users.noreply.github.com>
2023-12-21 11:09:55 +0000

    data type summary

---
+++ 01_basics/dataTypeSummary.js
@@ -0,0 +1,37 @@
+// Primitive
+// 7 types: String, Number, Boolean, null, undefined, symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id == anotherId);
+
+const bigNumber = 323656454n
+
+
+// Reference(non primitive)
+// Array, Objects, Functions
+
+const heros = ["shaktiman", "naagraj", "doga"]
+
+let myObj = {
+    name: "Sumit",
+    age : 21,
+}
+
+const myFunction = function(){
+    console.log("Hello World")
+}
+
+console.log(typeof anotherId )
+
+
+
+
+
