37248142b59964746a00aa4f94192a04c642a96d
SumitDongare <110616406+SumitDongare@users.noreply.github.com>
2023-12-21 09:40:18 +0000

    conversion operations

--- 01_basics/conversionOperation.js
+++ 01_basics/conversionOperation.js
@@ -16,4 +16,19 @@ let negValue = -value
 // console.log("1"+2+2);
 // console.log(1+2+"2");
 
+// console.log(+true)
+// console.log("")
+// console.log(+"")
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2+2
+// console.table([num1, num2, num3])
+
+let gameCounter = 100
+++gameCounter
+// console.log(gameCounter)
+
+
+
 
