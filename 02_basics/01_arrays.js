6dc6e83994a14e28a4cfdf12d3f077077a956d37
SumitDongare <110616406+SumitDongare@users.noreply.github.com>
2023-12-22 13:35:13 +0000

    array

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,11 @@
+const myarr = [0, 1, 2, 3, 4, 5, 6];
+const myarr2 = new Array(1, 2, 3)
+console.log(myarr[0]);
+
+
+// Arrays Methods
+// myarr.push(7);
+// myarr.pop();
+myarr.unshift(9)
+myarr.shift()
+console.log(myarr);
