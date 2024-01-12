d15b04a10a21e569f03d657e5101253481039d65
SumitDongare <110616406+SumitDongare@users.noreply.github.com>
2024-01-12 10:14:59 +0000

    control flow is done

---
+++ 04_control_flow/one.js
@@ -0,0 +1,26 @@
+// const balance = 1000;
+
+// if(balance < 500){
+//     console.log("less than 500");
+
+// }else if(balance < 750){
+//     console.log("less than 750");
+
+// } else if(balance < 900){
+//     console.log("less than 900")
+// }else{
+//     console.log("less than 1200")
+// }
+
+const userLoggedIn = true;
+const debitCard = true;
+const loggedInFromGoogle = false;
+const loggedInFromEmail = true;
+
+if(userLoggedIn && debitCard && 2==2){
+    console.log("Allow to buy course");
+}
+
+if(loggedInFromGoogle || loggedInFromEmail){
+    console.log("User logged in");
+}
